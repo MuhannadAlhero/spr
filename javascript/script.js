a528070f8f3a0e60799509d5fcdfeb6ce4ce4474
MuhannadAlhero <sssudi2012@gmail.com>
2025-07-21 01:50:19 +0300

    60% wiht out animation

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+// for menulist icon nav bar 
+
+// التحكم بالقائمة
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+const navLinks = document.getElementById("menulist");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.add("active");
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "inline-block";
+});
+
+closeIcon.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menuIcon.style.display = "inline-block";
+    closeIcon.style.display = "none";
+});
+const langBtn = document.getElementById("lang-btn");
+const langMenu = document.getElementById("lang-menu");
+//   الخاص بDroplist
+// يفتح ويغلق القائمة
+langBtn.addEventListener("click", () => {
+    langMenu.classList.toggle("show");
+});
+
+// لما تضغط على أي خيار من القائمة → تغلق
+langMenu.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        langMenu.classList.remove("show");
+    });
+});
+
+// إذا ضغطت برا القائمة → تغلق
+window.addEventListener("click", function (e) {
+    if (!langBtn.contains(e.target) && !langMenu.contains(e.target)) {
+        langMenu.classList.remove("show");
+    }
+});
+
+// Vision,more animation Setion 
